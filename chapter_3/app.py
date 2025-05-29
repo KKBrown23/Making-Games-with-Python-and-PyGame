fcf702c6fdd2eb99bcf5e4c1c8b3d56d1a0240ff
KKBrown <funkeadetutu@gmail.com>
2025-05-29 19:17:34 +0100

    beta 1

--- chapter_3/app.py
+++ chapter_3/app.py
@@ -1,12 +1,14 @@
 import pygame, sys
 from pygame.locals import *
-import random
+import random, time
 
 CANVAS_WIDTH = 600
 CANVAS_HEIGHT = 400
 COL_NUMBER = 6
 ROW_NUMBER = 4
-
+BOX_W = CANVAS_WIDTH / COL_NUMBER
+BOX_H = CANVAS_HEIGHT / ROW_NUMBER
+OPENED_SHAPE = None
 GRAY = (100, 100, 100)
 NAVYBLUE = ( 60, 60, 100)
 WHITE = (255, 255, 255)
@@ -26,7 +28,7 @@ OVAL = 'oval'
 
 COLOUR_LIST = [RED, GRAY, NAVYBLUE, WHITE, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN]
 SHAPE_LIST = [DONUT, SQUARE, DIAMOND, LINES, OVAL]
-
+BOARD_DATA = []
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((CANVAS_WIDTH, CANVAS_HEIGHT))
 pygame.display.set_caption('Memory Game')
@@ -34,9 +36,9 @@ pygame.display.set_caption('Memory Game')
 
 
 
-def generate_board_data(nr_of_rows, nr_of_columns):
-    assert nr_of_rows * nr_of_columns % 2 == 0, 'Number of boxes needs to be even'
-    number_unique_shapes = int((nr_of_rows * nr_of_columns)/2)
+def generate_board_data(ROW_NUMBER, COL_NUMBER):
+    assert ROW_NUMBER * COL_NUMBER % 2 == 0, 'Number of boxes needs to be even'
+    number_unique_shapes = int((ROW_NUMBER * COL_NUMBER)/2)
     ran_shape_list = []
     # find a way to generate unique random shapes
     counter = 0
@@ -46,24 +48,22 @@ def generate_board_data(nr_of_rows, nr_of_columns):
         ran_index = random.randint(0, len(SHAPE_LIST)-1)
         random_shape = SHAPE_LIST[ran_index]
         new_shape = {
-            'row': None,
-            'column': None,
             'shape':  random_shape, 
             'colour':  random_colour,
         }
         # check if the shape is generated before
         is_unique = check_unique_shape_colour(new_shape, ran_shape_list)
         if not is_unique:
-            counter -= 1
+            continue
+
         ran_shape_list.append(new_shape)
         ran_shape_list.append(new_shape)
         counter += 1
     random.shuffle(ran_shape_list)
-    for row in range(0,nr_of_rows):
-        for column in range(0,nr_of_columns):
-            ran_shape_list[(row + 1) * column]['row'] = row
-            ran_shape_list[(row + 1) * column]['column'] = column
-            print(ran_shape_list[(row + 1) * column])
+    global BOARD_DATA
+    BOARD_DATA = ran_shape_list
+    print(BOARD_DATA)
+
 def check_unique_shape_colour(new_shape, ran_shape_list):
     for x in range(0, len(ran_shape_list)):
         ran_shape = ran_shape_list[x]
@@ -72,25 +72,118 @@ def check_unique_shape_colour(new_shape, ran_shape_list):
     return True    
 
 def box_creation():
-    box_w = CANVAS_WIDTH / COL_NUMBER
-    box_h = CANVAS_HEIGHT / ROW_NUMBER
     for row_index in range(0, ROW_NUMBER):
         for col_index in range(0, COL_NUMBER):
-            x1 = col_index * box_w
-            y1 = row_index * box_h
-            x2 = col_index * box_w + box_w
-            y2 = row_index * box_h + box_h
+            x1 = col_index * BOX_W
+            y1 = row_index * BOX_H
+            x2 = col_index * BOX_W + BOX_W
+            y2 = row_index * BOX_H + BOX_H
             pygame.draw.rect(DISPLAYSURF, 
                              (255, 255, 255 ),
             ((x1,y1), (x2,y2)) , width=3)
+def show_shape(row_index, col_index):
+    shape_index = row_index*COL_NUMBER + col_index
+    current_shape = BOARD_DATA[shape_index]
+    shape_coords = get_coords(row_index, col_index)
+    quarter = int(BOX_W * 0.25) # syntactic sugar
+    half = int(BOX_H * 0.5) # syntactic sugar
+    
+    if(current_shape['shape'] == OVAL):
+        pygame.draw.ellipse(DISPLAYSURF, current_shape['colour'], 
+                            (shape_coords['x1'], shape_coords['y1'] + BOX_H/4 , BOX_W, BOX_H/2),
+                              0)
+    if(current_shape['shape'] == DONUT):
+        pygame.draw.circle(DISPLAYSURF, current_shape['colour'], (shape_coords['x1'] + half, shape_coords['y1'] + half), half - 5)
+        pygame.draw.circle(DISPLAYSURF, (0,0,0), (shape_coords['x1'] + half, shape_coords['y1'] + half), quarter - 5)
+    if(current_shape['shape'] == SQUARE):
+        pygame.draw.rect(DISPLAYSURF, current_shape['colour'], (shape_coords['x1']+BOX_W/4, shape_coords['y1']+BOX_H/4, BOX_W/2, BOX_H/2), 0)
+    # draw the other shapes keep in mind that quater = BOX_W/4 half = BOX_W / 2, left => shap_coords['x1], top => shape_coords['y1]
+    if(current_shape['shape'] == DIAMOND):
+        pygame.draw.polygon(DISPLAYSURF, current_shape['colour'], ((shape_coords['x1'] + BOX_W / 2, shape_coords['y1']), (shape_coords['x1']
+        + BOX_W - 1, shape_coords['y1'] + BOX_W / 2), (shape_coords['x1'] + BOX_W / 2, shape_coords['y1'] + BOX_W - 1), (shape_coords['x1'],  shape_coords['y1'] + 
+        BOX_W / 2)))
+    if(current_shape['shape'] == LINES):
+        for i in range(0, int(BOX_W), 4):
+            pygame.draw.line(DISPLAYSURF, current_shape['colour'], (shape_coords['x1'] , shape_coords['y1'] + i), (shape_coords['x1'] + 
+            i, shape_coords['y1'] ))
+            pygame.draw.line(DISPLAYSURF, current_shape['colour'], (shape_coords['x1'] + i, shape_coords['y1'] + BOX_W 
+            - 1), (shape_coords['x1']+ BOX_W - 1, shape_coords['y1'] + i))
+    if(current_shape['shape'] == OVAL):
+        pygame.draw.ellipse(DISPLAYSURF, current_shape['colour'], (shape_coords['x1'], shape_coords['y1'] + BOX_W / 4, 
+        BOX_W, BOX_W / 2))
+    pygame.display.update()
+    check_shape(row_index, col_index)
+
+def check_shape(row_index, col_index):
+
+    global OPENED_SHAPE
+    shape_index = row_index*COL_NUMBER + col_index
+    current_shape = BOARD_DATA[shape_index]
+    shape_coords = get_coords(row_index, col_index)
+    
+    if OPENED_SHAPE != None:
+        pygame.time.wait(1000)
+
+        opened_shape_coords = get_coords(OPENED_SHAPE['row_index'], OPENED_SHAPE['col_index'])
+        if current_shape['shape'] == OPENED_SHAPE['shape'] and current_shape['colour'] == OPENED_SHAPE['colour']:
+            OPENED_SHAPE = None
+        else:
+            OPENED_SHAPE = None
+            pygame.draw.rect(DISPLAYSURF, 
+                             (0, 0, 0 ),
+            (shape_coords['x1'],shape_coords['y1'], BOX_W, BOX_H), 0 )
+            pygame.draw.rect(DISPLAYSURF, 
+                             (255, 255, 255 ),
+            (shape_coords['x1'],shape_coords['y1'], BOX_W, BOX_H), width=3)
+            pygame.draw.rect(DISPLAYSURF, 
+                             (0, 0, 0 ),
+            (opened_shape_coords['x1'],opened_shape_coords['y1'], BOX_W, BOX_H) , 0)
+            pygame.draw.rect(DISPLAYSURF, 
+                             (255, 255, 255 ),
+            (opened_shape_coords['x1'],opened_shape_coords['y1'], BOX_W, BOX_H), width=3)
+    else:
+        OPENED_SHAPE = {
+            'shape': current_shape['shape'],
+            'colour': current_shape['colour'],
+            'row_index': row_index,
+            'col_index': col_index
+        }
+    # check if we have an opened shape 
+    #       if yes -> check if they match
+    #           if yes-> leave open + cleare the opened-shape variable
+    #           if not -> close then + clear the opened-shape
+    # if don't have opened-card
+    #   initialize opened-card 
+def get_coords(row_index, col_index):
+    x1 = col_index * BOX_W
+    y1 = row_index * BOX_H
+    x2 = col_index * BOX_W + BOX_W
+    y2 = row_index * BOX_H + BOX_H
+    return {'x1': x1,
+            'y1': y1,
+            'x2': x2,
+            'y2': y2}
 
 box_creation()
-
-generate_board_data(6,4)
+generate_board_data(ROW_NUMBER, COL_NUMBER)
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            pos = event.pos
+            row_index = int(pos[1] / BOX_H)
+            col_index = int(pos[0] / BOX_W)
+            print( f'Row: {row_index} Col: {col_index}')
+            # HMRK
+            # show the shape
+            show_shape(row_index, col_index)
+            # pygame.time.delay(2000)
+            # Find the data for the shape in the board data
+            # Search by row index and column index
+            # Find the x1, y1, x2 and y2 using the formulas 
+            # Draw the shape on the screen
+            # check_combination
     pygame.display.update()
 
