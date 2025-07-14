c6ee3db60ee07f0bfe39c1d56c44942723b40a5a
KKBrown <funkeadetutu@gmail.com>
2025-07-14 18:27:19 +0100

    lightning done

--- chapter_5/app.py
+++ chapter_5/app.py
@@ -34,17 +34,76 @@ BUTTONSIZE + BUTTONGAPSIZE, BUTTONSIZE, BUTTONSIZE)
 
 DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.draw.rect(DISPLAYSURF, BLUE, ((XMARGIN, YMARGIN),(BUTTONSIZE,BUTTONSIZE)))
-pygame.draw.rect(DISPLAYSURF, BRIGHTGREEN,
+pygame.draw.rect(DISPLAYSURF, GREEN,
                   ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE,
                     YMARGIN), 
                     (BUTTONSIZE,BUTTONSIZE)))
-pygame.draw.rect(DISPLAYSURF, BRIGHTYELLOW, ((XMARGIN, YMARGIN +
+pygame.draw.rect(DISPLAYSURF, YELLOW, ((XMARGIN, YMARGIN +
                                               BUTTONSIZE + BUTTONGAPSIZE),
                                               (BUTTONSIZE,BUTTONSIZE)))
-pygame.draw.rect(DISPLAYSURF, BRIGHTRED, ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, 
+pygame.draw.rect(DISPLAYSURF, RED, ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, 
                                            YMARGIN + BUTTONSIZE + BUTTONGAPSIZE),(BUTTONSIZE,BUTTONSIZE)))
 pygame.init()
 clock = pygame.time.Clock()
+pygame.display.update()
+
+# Input: the amount of random moves
+# Calculations: Make a loop that repeats as many times as the amount
+#                   for each repeat:
+#                       generate a random number then add to the array  
+#                       range should be 1-4
+# Output: Array of random moves
+array_of_random_moves = []
+
+def random_moves(amount_of_random_moves):
+    for x in range(0, amount_of_random_moves):
+        random_number = random.randint(1,4)
+        array_of_random_moves.append(random_number)
+    return array_of_random_moves
+
+def lighting_squares(array_of_random_moves):
+    for x in range(0, len(array_of_random_moves)):
+        pygame.time.wait(1000)
+        random_move = array_of_random_moves[x]
+        if random_move == 1:
+            pygame.draw.rect(DISPLAYSURF, BRIGHTBLUE, ((XMARGIN, YMARGIN),(BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pygame.draw.rect(DISPLAYSURF, BLUE, ((XMARGIN, YMARGIN),(BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+
+        if random_move == 2:
+            pygame.draw.rect(DISPLAYSURF, BRIGHTGREEN,
+                  ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE,
+                    YMARGIN), 
+                    (BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pygame.draw.rect(DISPLAYSURF, GREEN,
+                  ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE,
+                    YMARGIN), 
+                    (BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+        if random_move == 3:
+            pygame.draw.rect(DISPLAYSURF, BRIGHTYELLOW, ((XMARGIN, YMARGIN +
+                                              BUTTONSIZE + BUTTONGAPSIZE),
+                                              (BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pygame.draw.rect(DISPLAYSURF, YELLOW, ((XMARGIN, YMARGIN +
+                                              BUTTONSIZE + BUTTONGAPSIZE),
+                                              (BUTTONSIZE,BUTTONSIZE)))
+
+            pygame.display.update()
+        if random_move == 4:
+            pygame.draw.rect(DISPLAYSURF, BRIGHTRED, ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, 
+                                           YMARGIN + BUTTONSIZE + BUTTONGAPSIZE),(BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pygame.draw.rect(DISPLAYSURF, RED, ((XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, 
+                                           YMARGIN + BUTTONSIZE + BUTTONGAPSIZE),(BUTTONSIZE,BUTTONSIZE)))
+            pygame.display.update()
+lighting_squares(random_moves(5))
 while True:
     clock.tick(FPS)
 
